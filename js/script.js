e882f80a00a65a65c9001921e9dcd5b0c1ed4915
SashaDynia <sasaivadim@gmail.com>
2024-10-08 19:04:58 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let celsius = 20;
+let fahrenheit = (celsius / 5) * 9 + 32;
+console.log(fahrenheit);
+
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+console.log(daysInMonth);
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let health = 200;
+let energy = 100;
+console.log(health - 50);
+console.log(energy - 50);
+
+let purchase = 200;
+let discount = 10% purchase;
+let discountedPurchase = purchase - discount;
+console.log(discountedPurchase);
+
+let num = 9.6;
+console.log(Math.floor(num));
+
+let number = "30.3";
+console.log(parseFloat(number));
+
+let wholeNumber = "20.5";
+console.log(parseInt(wholeNumber));
+
+let sqrt = 16;
+console.log(Math.sqrt(sqrt));
+
+let value = 9;
+let stringWithNumber = "33";
+console.log(parseInt(stringWithNumber));
+console.log(toString(value));
